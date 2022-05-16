f2a689bcc7b9c9789d4895573283ffe93b16a358
AlbertLin <chlin701005@gmail.com>
2022-05-16 17:04:49 +0800

    20220516 jpa practice

--- src/main/java/com/uuu/springboot/demo/demo4/Runner1.java
+++ src/main/java/com/uuu/springboot/demo/demo4/Runner1.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @Slf4j
 public class Runner1 implements CommandLineRunner {
@@ -13,7 +15,37 @@ public class Runner1 implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        // insert
         fillBeverage();
+        // select
+        dumpAllBeverage();
+        // modify
+        modifyGreenTea();
+        dumpAllBeverage();
+        // delete
+        deleteRecord();
+        dumpAllBeverage();
+    }
+
+    private void deleteRecord() {
+        Optional<Beverage> optBeverage = repository.findById(8L);
+        Beverage b = optBeverage.get();
+        repository.delete(b);
+        log.info("----*****------***********-----------");
+    }
+
+    private void modifyGreenTea() {
+        Optional<Beverage> optBeverage = repository.findById(6L);
+        Beverage b = optBeverage.get();
+        b.setTitle("Kyoto Hot Green Tea");
+        repository.save(b);
+        log.info("------------------------");
+    }
+
+    private void dumpAllBeverage() {
+        for(Beverage b : repository.findAll()) {
+            log.info("get a beverage:{}", b);
+        }
     }
 
     private void fillBeverage() {
